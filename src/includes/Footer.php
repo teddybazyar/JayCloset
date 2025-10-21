47d84472072293eaef60776bee25afbb93b7858e
Kylie (Kanade) Ueda <uedak@etown.edu>
2025-10-20 23:08:00 -0400

    footer edited

--- src/includes/Footer.php
+++ src/includes/Footer.php
@@ -3,12 +3,7 @@
     class Footer {
 
         private $text;
-        // private $socialLinks = 'Connect with us:<br>
-        // <a href="https://www.facebook.com/etowncollege/" class="fa fa-facebook"></a>
-        // <a href="https://www.instagram.com/etowncollege/" class = "fa fa-instagram"></a>
-        // <a href="https://twitter.com/EtownCollege?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" class = "fa fa-twitter"></a>
-        // <a href="https://www.linkedin.com/school/elizabethtown-college/" class = "fa fa-linkedin"></a>';
-
+    
         public function __construct($text) {
 
             $this->text = $text;
