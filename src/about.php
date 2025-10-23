95946a73e25bde11fe39711c706b955abbd5405b
isabellaaaa111 <edwardsi@etown.edu>
2025-10-23 08:55:09 -0400

    About Page

--- src/about.php
+++ src/about.php
@@ -14,7 +14,7 @@ $footer =new Footer("Jay Closet 2025");
 <!DOCTYPE html>
 <html lang='en'>
   <head>
-    
+  <link rel="icon" type="image/jpg" href="/JayCloset/images/images/about_photos/Edwards_Isabella.JPG"/>  
   <link rel="stylesheet" href="css/style.css">
     <title> About - Jay Closet </title>
   </head>
@@ -30,7 +30,34 @@ $footer =new Footer("Jay Closet 2025");
             <p>   
             This JayCloset project was founded by four members from CS 341 Software Engineering class of Fall 2025.  
             </p>
-          </div>      
+          </div>     
+          <div class="team-members">
+            <div class="member1">
+              <h2> Teddy Bazyar </h2>
+               <p> <img src="" alt="" width="300" height="250"> Insert bio here</p>
+            </div>
+            <br>
+            <br>
+            <div class="member2">
+              <h2> Isabella Edwards </h2>
+              <p> <img src="/JayCloset/images/about_photos/Edwards_Isabella.JPG" alt="" width="300" height="250"> Insert bio here</p>
+            </div>
+            <br>
+            <br>
+            <div class="member3">
+              <h2> Isabella La Face </h2>
+              <p> <img src="" alt="" width="300" height="250"> Insert bio here</p>
+            </div>
+            <br>
+            <br>
+            <div class="member4">
+              <h2> Kylie(Kanade) Ueda </h2>
+              <p> <img src="" alt="" width="300" height="250"> Insert bio here</p>
+            </div>
+            <br>
+            <br>
+          </div>
+              
         </section>
     </main>
     <?php echo $footer->render(); ?>
