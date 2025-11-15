d38dc5ba0524b371396c36ad361a12eed412ecef
Kylie (Kanade) Ueda <uedak@etown.edu>
2025-11-15 00:04:01 -0500

    Got filter work and added more images

--- src/closet.php
+++ src/closet.php
@@ -9,8 +9,23 @@ require_once __DIR__ . '/includes/jayclosetdb.php';
 $nav = new Navbar();
 $footer = new Footer("Jay Closet 2025");
 $title = 'Closet - Jay Closet';
+$clothesImgDir = '../images/items/';
 
-$getItemsSQL = "SELECT * FROM descript;";
+// Filter //
+$filterGender = $_GET['gender'] ?? null;
+$filterCategory = $_GET['categories'] ?? null;
+
+# SQL - gender, category, and all
+if ($filterGender) {
+    $getItemsSQL = "SELECT * FROM descript WHERE reserved = 0 AND gender = '" . $filterGender . "'";
+}
+else if ($filterCategory) {
+    $getItemsSQL = "SELECT * FROM descript WHERE reserved = 0 AND categories = '" . $filterCategory . "'";
+}
+else {
+    $getItemsSQL = "SELECT * FROM descript WHERE reserved = 0";
+}
+
 $allItems = jayclosetdb::getDataFromSQL($getItemsSQL);
 ?>
 
@@ -28,31 +43,58 @@ $allItems = jayclosetdb::getDataFromSQL($getItemsSQL);
 <?php $nav->display(); ?>
 
 <div class="sidenav">
-    <a href="#">Women</a>
-    <a href="#">Men</a>
-    <a href="#">Unisex</a>
-    <a href="#">Tops</a>
-    <a href="#">Bottoms</a>
-    <a href="#">Jacket</a>
-    <a href="#">Shoes</a>
-    <a href="#">Dress</a>
-    <a href="#">Others</a>
+    <h4><a href="closet.php">All Items</a></h4>
+    <h4>Search by Gender</h4>
+    <a href="closet.php?gender=Women">Women</a>
+    <a href="closet.php?gender=Men">Men</a>
+    <a href="closet.php?gender=Unisex">Unisex</a>
+    <h4>Search by Category</h4>
+    <a href="closet.php?categories=Tops/Blouse">Tops/Blouse</a>
+    <a href="closet.php?categories=Sweater/Vest/Cardigan">Sweater/Vest/Cardigan</a>
+    <a href="closet.php?categories=Jacket">Jacket</a>
+    <a href="closet.php?categories=Bottoms">Bottoms</a>
+    <a href="closet.php?categories=Shoes">Shoes</a>
+    <a href="closet.php?categories=Dress">Dress</a>
+    <a href="closet.php?categories=Others">Others</a>
+    <h4>Search by keyword</h4>
+    <!-- add search field -->
 </div>
 
 <div class="main-content"> <!-- EVERYTHING inside MAIN will move right -->
+    <!-- display filter -->
+    <h1> 
+        <?php
+        if ($filterGender) {
+            echo htmlspecialchars($filterGender);
+        } elseif ($filterCategory) {
+            echo htmlspecialchars($filterCategory);
+        } else {
+            echo "All Items";
+        }
+        ?>
+    </h1>
 
-    <h2>All Items</h2>
 
     <?php if (!empty($allItems)): ?>
         <div class="item-container">
             <?php foreach ($allItems as $row): ?>
                 <div class="itembox">
-                    <img src="../images/items/missingImages.png" class="item-image">
+                    <?php
+                        $clothesImgPath = $clothesImgDir . $row["itemID"] . "/" . $row['itemID'] . "-1.png";
+                        if (file_exists($clothesImgPath) == 0) {
+                            $clothesImgPath = $clothesImgDir . "missingImages.png";
+                        }
+                    ?>
+                    <img src="<?= htmlspecialchars($clothesImgPath) ?>" class='item-image'>
                     <h3><?= htmlspecialchars($row["description_of_item"]) ?></h3>
-                    <p>Color: <?= htmlspecialchars($row["color"]) ?></p>
-                    <p>Size: <?= htmlspecialchars($row["size"]) ?></p>
-                    <p>Gender: <?= htmlspecialchars($row["gender"]) ?></p>
-                    <p>Availability: <?= $row["reserved"] ? "Reserved" : "Available" ?></p>
+                    <p><strong>Color: </strong><?= htmlspecialchars($row["color"]) ?></p>
+                    <p><strong>Size: </strong><?= htmlspecialchars($row["size"]) ?></p>
+                    <p><strong>Gender: </strong><?= htmlspecialchars($row["gender"]) ?></p>
+                    <p><strong>Category: </strong><?= htmlspecialchars($row["categories"]) ?></p>
+                    <div class="button-wrapper">
+                        <button type='button'>Add to Cart</button>
+                    </div>
+                    
                 </div>
             <?php endforeach; ?>
         </div>
